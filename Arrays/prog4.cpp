fd1dfe75a1b5fdc4621abbc011d80e6a8987d7a0
Pratimapal1209 <pratimap1209@gmail.com>
2023-04-23 10:57:19 +0530

    reverse an array - method 1 & method 2

--- Arrays/prog4.cpp
+++ Arrays/prog4.cpp
@@ -1,3 +1,6 @@
+//reverse an array
+
+
 #include<iostream>
 using namespace std;
 
@@ -15,4 +18,42 @@ int main(){
     for(int i = n-1; i>=0;i--){
         cout<<arr[i]<<" ";
     }
+}
+
+
+//method 2
+
+
+
+
+
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int n ;
+    cin>>n;
+    int arr[n];
+     for (int i =0;i<n;i++){
+        cin>>arr[i];
+     }
+
+     
+
+     int low = 0, high = n-1, temp;
+     while(low<high){
+        int temp = arr[low];
+        arr[low]=arr[high];
+        arr[high] = temp ;
+        low++;
+        high--;
+
+        
+     }
+     cout<<"array after reversal:"<<endl;
+     for(int i = 0; i<n;i++){
+     cout << arr[i]<<" ";
+     }
 }
