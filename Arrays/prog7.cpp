13618cf23a21c681dfba4ad2d8e4874e7e0e69d4
Pratimapal1209 <pratimap1209@gmail.com>
2023-04-25 22:30:46 +0530

    move zeros to end

---
+++ Arrays/prog7.cpp
@@ -0,0 +1,16 @@
+// move zeros to end
+
+
+void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        for(int i= 0 ;i<n;i++){
+            if(nums[i] != 0){
+                swap(nums[i], nums[count]);
+                count++;
+            }
+
+        }
+        
+    }
+    
